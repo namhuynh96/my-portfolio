3ba735464e3c6ad692aff1ef7a51fa86e75fb653
Nam Huynh <nam@capogrecogroup.com>
2021-11-06 07:10:22 +0700

    Add path to serve coding test

--- portfolioServer.js
+++ portfolioServer.js
@@ -4,6 +4,12 @@ const path = require("path");
 const app = express();
 
 app.use(express.static(path.join("my-portfolio-public", "build")));
+app.use(express.static(path.join("remitano", "build")));
+
+app.use("/coding-test", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "remitano", "build", "index.html"));
+});
+
 app.use((req, res, next) => {
   res.sendFile(
     path.resolve(__dirname, "my-portfolio-public", "build", "index.html")
